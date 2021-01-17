4efd2fc4dc817a3e52033213ea9db6a09e95247e
Sudhanshu277 <palsudhanshu94@gmail.com>
2021-01-17 20:56:07 +0530

    pn

---
+++ js/get.js
@@ -0,0 +1,11 @@
+var element;
+
+element = document.body;
+console.log(element);
+
+element=document.getElementById('a').innerText;
+element=document.getElementById('a').innerHTML;
+element=document.getElementById('a').getAttribute("style");
+element=document.getElementById('a').getAttributeNode("style");
+element=document.getElementById("b").attributes;
+element=document.getElementById("b").attributes[0];
---
+++ js/set.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById('a').innerHTML = "<center><h1>PNinfosys</h1></center>";
+document.getElementById('b').innerHTML = "<h1>phone:123445</h1>";
+document.getElementById('a').setAttribute("style","border:10px dotted red");
+document.getElementById('a').removeAttribute("style");
+document.querySelector("#a").innerHTML ="<center<h1>wow</h1></center>";
+document.querySelector(".c").innerHTML ="<center<h1>taj</h1></center>";
